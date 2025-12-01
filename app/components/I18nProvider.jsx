adde807fa350e89932f3b75df5522162b9635169
agusttin-ader <agusttin.ader@hotmail.com>
2025-12-01 00:06:52 -0300

    chore: remove final i18n component files

--- app/components/I18nProvider.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export default function I18nProvider({ children }) {
-  // No-op provider (i18n removed). Kept so imports don't break until files are cleaned.
-  return children;
-}
